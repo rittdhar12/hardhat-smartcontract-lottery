29dc469f244588deb9679ab9f67f364f0137ad0e
rittdhar12 <rittdhar12@gmail.com>
2024-01-18 10:45:10 -0500

    Updated deploy script

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,7 +1,7 @@
-const {ethers} = require("hardhat")
+const { ethers } = require("hardhat");
 const networkConfig = {
     default: {
-        name: "hardhat", 
+        name: "hardhat",
         keepersUpdateInterval: "30",
     },
     11155111: {
@@ -9,12 +9,12 @@ const networkConfig = {
         vrfCoordinatorV2: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625",
         entranceFee: ethers.parseEther("0.01"),
         gasLaneKeyHash: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
-        subscriptionId: "8536", 
-        callbackGasLimit: "5000000", 
-        interval: "30"
+        subscriptionId: "8536",
+        callbackGasLimit: "5000000",
+        interval: "30",
     },
     31337: {
-        name: "hardhat",
+        name: "localhost",
         subscriptionId: "588",
         gasLaneKeyHash: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c", // 30 gwei
         interval: "30",
@@ -25,4 +25,6 @@ const networkConfig = {
 
 const developmentChains = ["hardhat", "localhost"];
 
-module.exports = { networkConfig, developmentChains };
+const VERIFICATION_BLOCK_CONFIRMATIONS = 6;
+
+module.exports = { networkConfig, developmentChains, VERIFICATION_BLOCK_CONFIRMATIONS };
